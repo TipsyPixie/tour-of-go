61b7b56b1da1c1c67c893bb765c9593a089a6caa
S.Hwang <lotsofluck4m@gmail.com>
2018-09-22 04:03:56 +0900

    Update errors.go

--- errors.go
+++ errors.go
@@ -12,6 +12,11 @@ func (e ErrNegativeSqrt) String() string {
 }
 
 func (e ErrNegativeSqrt) Error() string {
+    /*
+     * Calling fmt.Println() or fmt.Sprint() causes infinite loop here.
+     * This is because fmt.print() tries to convert e into string by calling Error().
+     * In a similar manner, this infinite loop issue occurs upon calling fmt.Sprint() or fmt.Printf() with %s format.
+     */
 	return fmt.Sprintf("Cannot Sqrt negative number: %.5f", e)
 }
 
