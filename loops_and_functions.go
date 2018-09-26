a35f8cb7f9061b68a273759f7098bac2a38b8d43
S.Hwang <lotsofluck4m@gmail.com>
2018-09-26 14:03:31 +0900

    Update loops_and_functions.go

--- loops_and_functions.go
+++ loops_and_functions.go
@@ -11,7 +11,7 @@ func Sqrt(x float64) float64 {
     // Go does not support implicit type casting.
 	z := float64(initialValue)
 
-    // Note that Go does not support syntactic sugars: neither while nor forEach
+    // Note that Go supports neither while nor forEach
 	for i := 0; i < 10000; i++ {
 		z -= (math.Pow(z, 2) - x) / (2 * z)
 	}
